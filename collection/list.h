d1f17bbdedddf1be5014ef94afe0034f1d6795d1
namsic <kimnj3050@gmail.com>
2023-02-18 19:50:29 +0900

    feat: Add list interface
    Related
    - #4
    
    Add methods
    - size
    - get
    - insert
    - remove

---
+++ collection/list.h
@@ -0,0 +1,12 @@
+#ifndef __COLLECTION_LIST_H__
+#define __COLLECTION_LIST_H__
+
+struct ListInterface{
+  unsigned int (*size)   (const struct ListInterface *self);
+  void*        (*get)    (const struct ListInterface *self, const unsigned int index);
+
+  void         (*insert) (struct ListInterface *self, const unsigned int index, void* value);
+  void         (*remove) (struct ListInterface *self, const unsigned int index);
+};
+
+#endif  // __COLLECTION_LIST_H__
