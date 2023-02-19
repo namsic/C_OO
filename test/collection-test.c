1ccb64a677893c81690c8a6eac3f52d73fedf5c3
namsic <kimnj3050@gmail.com>
2023-02-19 14:45:46 +0900

    test: Use cmake and add custom target for test
    Related
    - #1
    - #3: Add collection-test.c for collection test
    - #4: Add listTest() to collection-test.c
    - #6: Test linked list
    
    Add Int() to util for easy use of int pointer

---
+++ test/collection-test.c
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "../util.h"
+#include "../collection/list.h"
+
+static void listTest(struct ListInterface *list)
+{
+  assert(list->size(list) == 0);
+
+  int *elem;
+
+  // insert to empty
+  list->insert(list, 0, Int(10));
+  assert(list->size(list) == 1);
+  elem = list->get(list, 0); assert(*elem == 10);
+
+  // insert to head
+  list->insert(list, 0, Int(20));
+  assert(list->size(list) == 2);
+  elem = list->get(list, 0); assert(*elem == 20);
+  elem = list->get(list, 1); assert(*elem == 10);
+
+  // insert to mid
+  list->insert(list, 1, Int(30));
+  assert(list->size(list) == 3);
+  elem = list->get(list, 0); assert(*elem == 20);
+  elem = list->get(list, 1); assert(*elem == 30);
+  elem = list->get(list, 2); assert(*elem == 10);
+
+  // insert to tail
+  list->insert(list, 3, Int(40));
+  assert(list->size(list) == 4);
+  elem = list->get(list, 0); assert(*elem == 20);
+  elem = list->get(list, 1); assert(*elem == 30);
+  elem = list->get(list, 2); assert(*elem == 10);
+  elem = list->get(list, 3); assert(*elem == 40);
+
+  // remove to head
+  elem = list->remove(list, 0);
+  assert(*elem == 20 && list->size(list) == 3);
+  free(elem);
+  elem = list->get(list, 0); assert(*elem == 30);
+  elem = list->get(list, 1); assert(*elem == 10);
+  elem = list->get(list, 2); assert(*elem == 40);
+
+  // remove to mid
+  elem = list->remove(list, 1);
+  assert(*elem == 10 && list->size(list) == 2);
+  free(elem);
+  elem = list->get(list, 0); assert(*elem == 30);
+  elem = list->get(list, 1); assert(*elem == 40);
+
+  // remove to tail
+  elem = list->remove(list, 1);
+  assert(*elem == 40 && list->size(list) == 1);
+  free(elem);
+  elem = list->get(list, 0); assert(*elem == 30);
+}
+
+int main()
+{
+  struct ListInterface *list = newLinkedList();
+  listTest(list);
+  free(list);
+
+  return 0;
+}
---
+++ util.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <assert.h>
+
+int* Int(int value)
+{
+  int *ret = malloc(sizeof(int));
+  assert(ret);
+  *ret = value;
+  return ret;
+}
